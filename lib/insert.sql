bba57d8cd2a76f5978443fdfd304522971ecc8cc
Hunter <kolasa.hunter@aol.com>
2020-12-08 15:01:01 -0500

    commit tree a9aec9f798ce18230a3dd8d3203c96aaeea8ebfb

---
+++ lib/insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO users (name, age) VALUES ('Jeff', 23), ('Rachel', 27), ('Rachel', 27), ('Rachel', 27), ('Rachel', 27), ('Rachel', 27), ('Rachel', 27),
+     ('Matt', 18), ('Fred', 38), ('Sara', 50), ('Jill', 32), ('Andrew', 28), ('Dan', 30), ('Smith', 99), ('Octavius', 21), ('Nick', 19), ('Anthony', 60),
+     ('Dylan', 33), ('Mike', 44), ('Lily', 36);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Save the turtles', 'environment', 10000, 05-08-2020, 05-08-2022), 
+    ('Pay for cyberpunk', 'video games', 60, 12-08-2020, 12-10-2020), ('Help kids learn english', 'charity', 10000, 06-10-2020, 10-10-2023), 
+    ('Fixes cyberpunk bugs', 'video games', 18000, 12-10-2020, 11-08-2021), ('Plant trees', 'environment', 100000, 01-10-2020, 07-08-2030), 
+    ('Support healthcare workers', 'health', 2500000, 01-01-2020, 12-25-2020), ('Support my youtube channel', 'dumb', 10000, 02-12-2020, 03-12-2020), 
+    ('Find life in the stars', 'science', 31200000, 04-25-2012, 11-15-2035), ('Fix my lisp', 'health', 1, 12-08-2020, 12-09-2020), 
+    ('I don''t want to ever work again', 'health', 10000000, 08-29-1995, 08-29-2050); 
+INSERT INTO pledges (amount, user_id, project_id) VALUES (30, 9, 1), (100, 5, 2), (30, 9, 1), (30, 9, 1), (30, 9, 1), (30, 9, 1), (30, 9, 1), (30, 9, 1), 
+(30, 9, 1), (30, 9, 1), (30, 9, 1), (30, 9, 1), (30, 9, 1), (30, 9, 1), (30, 9, 1), (30, 9, 1), (30, 9, 1), (30, 9, 1), (30, 9, 1), (30, 9, 1), (30, 9, 1), 
+(30, 9, 1), (30, 9, 1), (30, 9, 1), (30, 9, 1), (30, 9, 1), (30, 9, 1), (30, 9, 1), (30, 9, 1), (30, 9, 1);  
